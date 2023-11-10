0172c0dd7e019b7f59650dc7fc27db488fa33462
Nick-Reichlin <nickreichlin@gmail.com>
2023-11-10 13:33:37 -0500

    Added coloring changes to sign in and sign up forms

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,14 @@
 import { FC, ReactNode } from 'react';
+import "@/styles/styles.css"
 
 interface AuthLayoutProps {
   children: ReactNode;
 }
 
+{/*<div className='bg-stone-700 p-10 rounded-md'>*/}
+
 const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
-  return <div className='bg-slate-200 p-10 rounded-md'>{children}</div>;
+  return <div className='form'>{children}</div>;
 };
 
 export default AuthLayout;
