946fff7430975226ab2f4980ea2c4d5cd7d1df08
Nick-Reichlin <nickreichlin@gmail.com>
2023-11-30 23:59:25 -0500

    Add retrieval to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,19 @@
 import "@/styles/styles.css"
 import React from "react"
+import { useState, useEffect } from 'react';
+import { prisma } from '@/lib/prisma'
 
+export default async function Home() {
+  const user = await prisma.user.findFirst({
+    where: {
+      email: 'nickreichlin@gmail.com'
+    }
+  })
 
-export default async function profile() {
-  <body>
-    <h1>Profile</h1>
-  </body>
+  return (
+    <main>
+      <div>Hello, {user?.name}</div>
+      <div>Email: {user?.email}</div>
+    </main>
+  )
 }
