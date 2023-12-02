089f96d3f53f15b98d89a292225bc88bcf7c2a3a
Enzi Lewis-Baranyai <enzi_lewis@icloud.com>
2023-12-02 17:46:55 -0500

    add files

---
+++ src/components/SpotifySignInButton.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactNode } from "react";
+import { Button } from "./ui/button";
+import { signIn } from "next-auth/react";
+
+interface SpotifySignInButtonProps {
+  children: ReactNode;
+}
+
+const SpotifySignInButton: FC<SpotifySignInButtonProps> = ({ children }) => {
+  const loginWithSpotify = () =>
+    signIn("spotify", { callbackUrl: "https://localhost:3000/admin" });
+
+  return (
+    <Button onClick={loginWithSpotify} className="w-full">
+      {children}
+    </Button>
+  );
+};
+
+export default SpotifySignInButton;
