c4ba6ac030e85e1008b1a2094f2ed00b6478ed40
Enzi Lewis-Baranyai <enzi_lewis@icloud.com>
2023-12-03 18:50:56 -0500

    add your music page changes

--- src/app/yourmusic/page.tsx
+++ src/app/yourmusic/page.tsx
@@ -1,10 +1,27 @@
-import "@/styles/styles.css"
-import React from "react"
-
+import "@/styles/styles.css";
+import React from "react";
 
 export default async function yourMusic() {
-  <body>
-    <h1>Connect Spotify Account</h1>
-  </body>
-
+  return (
+    <div className="">
+      <div className="text-center font-bold text-orange-400 text-xl text-align-center">
+        Insights About Your Music!
+      </div>
+      <div className="border-2 border-white m-5">
+        <div className="text-center text-orange-300 text-lg">
+          Your Top 5 Songs:{" "}
+        </div>
+      </div>
+      <div className="border-2 border-white m-5">
+        <div className="text-center text-orange-300 text-lg">
+          Recommended Songs:{" "}
+        </div>
+      </div>
+      <div className="border-2 border-white m-5">
+        <div className="text-center text-orange-300 text-lg">
+          Your Top 5 Artists:{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
